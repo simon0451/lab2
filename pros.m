b2252fb0e661cbf636a30a9d3306af0cfb4710e4
Simon <sapopecki@gmail.com>
2017-02-23 17:57:27 -0500

    thermocouple voltages converted to temperatures, methods 1 and 2 working

--- pros.m
+++ pros.m
@@ -1,4 +1,5 @@
 function [output] = pros(time,voltage,startingtime)
+load lab2part1variables.mat
 offset = 0;
 
 for i=1:length(time)
@@ -18,7 +19,7 @@ for i=1:length(time)
     end
 end
 %create new variables that start from t = 0 and only contain event data
-newtime=time(starttime:length(time))-time(starttime);
+newtime=time(starttime:length(time)); %-time(starttime); %the commented out portion starts time t=0 at the event initiation point
 
 newvoltage=voltage(starttime:length(time));
 
@@ -26,8 +27,11 @@ Tstart = time(starttime);
 
 Vstart = voltage(starttime);
 
+%when it is more useful to output a temperature instead of voltage
+tcv = (newvoltage*1000-betaHat(1))/betaHat(2); %°C
+
 
 % output = [tout,vout];
-output = [newtime,newvoltage];
+output = [newtime,tcv]; %trimmed: newtime,newvoltage. untrimmed: time,voltage
 end
 
